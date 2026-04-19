8a28fea6cf429aa9e0200787e03575eab344260f
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-19 21:20:24 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -15,7 +15,6 @@ export default async function handler(req, res) {
     });
 
   const data = await response.json();
-console.log("GROQ RESPONSE:", data);
     res.status(200).json(data);
 
   } catch (err) {
